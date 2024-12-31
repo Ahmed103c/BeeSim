00223e7493e2f4c295927110d6205d2c92f9e79c
Rzeigui Ahmed <ahmedrzeigui52@gmail.com>
2024-12-31 14:18:52 +0100

    Ajout Dossier models

---
+++ beesim/src/main/java/com/beesim/models/abeilleCreation.java
@@ -0,0 +1,5 @@
+package com.beesim.models;
+
+public class abeilleCreation {
+    
+}
--- beesim/src/test/java/com/beesim/SimTest.java
+++ beesim/src/test/java/com/beesim/SimTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class SimTest 
 {
     /**
      * Rigorous Test :-)
@@ -17,4 +17,6 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    
 }
