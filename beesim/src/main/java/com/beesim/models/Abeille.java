e4fa91a562faafe6b97d7faf94e8ae56d2de2969
mouadismaili <mouad.ismaili_m_hamdi@etu.uca.fr>
2025-01-01 11:08:18 +0100

    ajout de getters et setters

--- beesim/src/main/java/com/beesim/models/Abeille.java
+++ beesim/src/main/java/com/beesim/models/Abeille.java
@@ -4,9 +4,34 @@ public abstract class Abeille {
     protected int x;
     protected int y;
     protected int capaciteNectarPrise;
-    public int score;
+    protected int score;
     protected EtatAbeille Etat;
     // public Abeille(){
     //     this.Etat=new Repos();
     // }
+    int getScore(){
+        return score;
+    }
+    void setScore(int score){
+        this.score = score;
+    }
+    int getX() {
+        return x;
+    }
+    void setX(int x) {
+        this.x = x;
+    }
+    int getY() {
+        return y;
+    }
+    void setY(int y) {
+        this.y = y;
+    }
+    int getCapaciteNectarPrise() {
+        return capaciteNectarPrise;
+    }
+    void setCapaciteNectarPrise(int capaciteNectarPrise) {
+        this.capaciteNectarPrise = capaciteNectarPrise;
+    }
+
 }
